6d4fde1022ef7b2bf6350134974c430dc1256484
Josh Justice <jjustice@bignerdranch.com>
2017-06-27 16:49:38 -0400

    Use component

--- angular-component/src/app/app.component.ts
+++ angular-component/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <message-reverser [message]="'Hello, Angular!'"></message-reverser>
+  `,
 })
 export class AppComponent  { name = 'Angular'; }
--- angular-component/src/app/app.module.ts
+++ angular-component/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+import { MessageReverserComponent } from './message-reverser.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, MessageReverserComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
