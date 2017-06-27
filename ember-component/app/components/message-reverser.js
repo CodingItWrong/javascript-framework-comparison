8f2ac48780af874b8cea4be969ba08e2016695f7
Josh Justice <jjustice@bignerdranch.com>
2017-06-27 16:42:25 -0400

    Implement reverser component

--- ember-component/app/components/message-reverser.js
+++ ember-component/app/components/message-reverser.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    reverse() {
+      let message = this.get('message');
+      this.set('message', message.split('').reverse().join(''));
+    }
+  }
 });
