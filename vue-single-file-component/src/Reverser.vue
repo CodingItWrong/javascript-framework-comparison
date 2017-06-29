bc875dbe10eb811120a764aa6417c4ecdcffd1e0
Josh Justice <jjustice@bignerdranch.com>
2017-06-29 05:53:55 -0400

    Add reverser component

---
+++ vue-single-file-component/src/Reverser.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input v-model="mutableMessage">
+    <button v-on:click="reverseMessage">Reverse Message</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'reverser',
+    props: ['message'],
+    data() {
+      return {
+        mutableMessage: this.message
+      }
+    },
+    methods: {
+      reverseMessage() {
+        this.mutableMessage = this.mutableMessage.split('').reverse().join('')
+      }
+    }
+  }
+</script>
